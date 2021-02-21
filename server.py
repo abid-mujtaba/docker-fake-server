e9efbee826234fa3efb80e234a2b13ec59c2f985
Abid H. Mujtaba <abid.naqvi83@gmail.com>
2021-02-21 11:09:59 -0500

    Mock out raw.githubusercontent.com

--- server.py
+++ server.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def process_route():
     return "Hello, World!\n"
 
+@app.route("/ohmyzsh/ohmyzsh/<path:subpath>")
+def process_sub_path(subpath):
+    return f"Accessing subpath: {subpath}\n"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=443, ssl_context="adhoc")
